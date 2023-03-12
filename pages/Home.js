80430a8ca8c94e6e58702f8ff6cbd5499eb67828
domd11 <dominiquedesertb@gmail.com>
2023-03-12 16:57:59 -0400

    displaying bookmarkd

--- pages/Home.js
+++ pages/Home.js
@@ -1,9 +1,29 @@
+import { collection, getDocs } from 'firebase/firestore';
 import { useRouter } from 'next/router';
-import React from 'react'
-import { auth } from '../Firebase'
+import React, { useEffect, useState } from 'react'
+import { auth, db } from '../Firebase'
+import Post from './Components/Post';
 const HomePage = () => {
   const user = auth.currentUser; 
   const router = useRouter();
+
+  const [posts, setPosts] = useState([]); 
+
+  const getData = async () => {
+    const ref = collection(db, "tips-tricks"); 
+    await getDocs(ref).then((response) => {
+        setPosts(response.docs.map((data) => {
+            return {...data.data(), id: data.id}
+        }));
+    });
+
+    console.log(posts)
+}
+
+useEffect(() => {
+  getData();
+}, [])
+
   return (
     <div>
       {auth.currentUser ? (
@@ -11,6 +31,13 @@ const HomePage = () => {
           <h1>Hello, {user.displayName}!</h1>
             <h2>Bookmarked Forums:</h2>
             <h2>Bookmarked Tips & Tricks:</h2>
+            {posts.map(post => {
+              return (
+                <div>
+                {post.bookmarks.includes(user.email) ? <Post post={post} /> : "" }
+              </div>
+              )
+            })}
         </div>
       ) : ""}
     </div>
